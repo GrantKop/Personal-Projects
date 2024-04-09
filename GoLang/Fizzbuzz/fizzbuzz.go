c04f4c0c86cb2d4e06fa555dd64aeb62dfa9f900
Grant Kopczenski <grant.kop9@gmail.com>
2024-04-09 15:29:20 -0700

    Started chess project, added comment to fizzbuzz

--- GoLang/Fizzbuzz/fizzbuzz.go
+++ GoLang/Fizzbuzz/fizzbuzz.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+// This program will count from 1 to 100 but every factor of 3 prints 'Fizz' and every factor of 5 prints 'Buzz'
+// Factors of both 3 and 5 print 'FizzBuzz'
 func main() {
 
 	for i := 1; i <= 100; i++ {
